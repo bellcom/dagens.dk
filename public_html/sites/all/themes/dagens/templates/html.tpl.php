8dbf005ed18c8b00abe9ed60ffb8867513db0c90
Morten Nissen <mn@bellcom.dk>
2015-03-18 12:55:57 +0100

    Changed ad tags on front and article pages

--- public_html/sites/all/themes/dagens/templates/html.tpl.php
+++ public_html/sites/all/themes/dagens/templates/html.tpl.php
@@ -7,46 +7,82 @@
   <?php print $scripts; ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 
-<script type='text/javascript'>
-var googletag = googletag || {};
-googletag.cmd = googletag.cmd || [];
-(function() {
-var gads = document.createElement('script');
-gads.async = true;
-gads.type = 'text/javascript';
-var useSSL = 'https:' == document.location.protocol;
-gads.src = (useSSL ? 'https:' : 'http:') + 
-'//www.googletagservices.com/tag/js/gpt.js';
-var node = document.getElementsByTagName('script')[0];
-node.parentNode.insertBefore(gads, node);
-})();
-</script>
+  <?php if(drupal_is_front_page()) { ?>
+    <!-- BEGIN DFP header scripts on desktop frontpage -->
+    <script type='text/javascript'>
+      var googletag = googletag || {};
+      googletag.cmd = googletag.cmd || [];
+      (function () {
+        /*jslint browser:true */
+        /*jslint eqeq: true*/
+        var gads, useSSL, node;
+        gads = document.createElement('script');
+        gads.async = true;
+        gads.type = 'text/javascript';
+        useSSL = 'https:' == document.location.protocol;
+        gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
+        node = document.getElementsByTagName('script')[0];
+        node.parentNode.insertBefore(gads, node);
+      })();
+    </script>
 
+    <script type='text/javascript'>
+      googletag.cmd.push(function () {
+        googletag.defineSlot('/49662453/DagensDK_160x600_L', [160, 600], 'div-DagensDK_160x600_L').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDK_160x600_R', [160, 600], 'div-DagensDK_160x600_R').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDk_300x250_1', [300, 250], 'div-DagensDk_300x250_1').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDK_300x250_2', [300, 250], 'div-DagensDK_300x250_2').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDK_930x180_1', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-0').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDK_930x180_2', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-1').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDK_930x180_3', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-2').addService(googletag.pubads());
+        //FMA// googletag.defineSlot('/49662453/DagensDK_930x600_Floating', [930, 600], 'div-DagensDK_930x600_Floating').addService(googletag.pubads());
+        //FMA// googletag.defineSlot('/49662453/DagensDK_960x600_Floating', [960, 600], 'div-DagensDK_960x600_Floating').addService(googletag.pubads());
+        //FMA// googletag.defineSlot('/49662453/DagensDK_Wallpaper', [960, 600], 'div-DagensDK_Wallpaper').addService(googletag.pubads());
+        googletag.pubads().enableSingleRequest();
+        googletag.enableServices();
+      });
+    </script>
+    <!-- END DFP header scripts on desktop frontpage -->
+  <?php } else { ?>
+    <!-- BEGIN DFP header scripts on desktop article pages -->
+    <script type='text/javascript'>
+      var googletag = googletag || {};
+      googletag.cmd = googletag.cmd || [];
+      (function () {
+        /*jslint browser:true */
+        /*jslint eqeq: true*/
+        var gads, useSSL, node;
+        gads = document.createElement('script');
+        gads.async = true;
+        gads.type = 'text/javascript';
+        useSSL = 'https:' == document.location.protocol;
+        gads.src = (useSSL ? 'https:' : 'http:') + '//www.googletagservices.com/tag/js/gpt.js';
+        node = document.getElementsByTagName('script')[0];
+        node.parentNode.insertBefore(gads, node);
+      })();
+    </script>
 
-<script type='text/javascript'>
-googletag.cmd.push(function() {
-	googletag.defineSlot('/49662453/DagensDK_160x600_L', [160, 600], 'div-DagensDK_160x600_L').addService(googletag.pubads());
-	googletag.defineSlot('/49662453/DagensDK_160x600_R', [160, 600], 'div-DagensDK_160x600_R').addService(googletag.pubads());
-	googletag.defineSlot('/49662453/DagensDk_250x250_Article_1', [250, 250], 'div-DagensDk_250x250_Article_1').addService(googletag.pubads());
-	googletag.defineSlot('/49662453/DagensDk_250x250_Article_2', [250, 250], 'div-DagensDk_250x250_Article_2').addService(googletag.pubads());
-	googletag.defineSlot('/49662453/DagensDk_250x250_Article_3', [250, 250], 'div-DagensDk_250x250_Article_3').addService(googletag.pubads());
-	googletag.defineSlot('/49662453/DagensDk_300x250_1', [300, 250], 'div-DagensDk_300x250_1').addService(googletag.pubads());
-	googletag.defineSlot('/49662453/DagensDK_300x250_2', [300, 250], 'div-DagensDK_300x250_2').addService(googletag.pubads());
-	googletag.defineSlot('/49662453/DagensDK_930x180_1', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-0').addService(googletag.pubads());
-	googletag.defineSlot('/49662453/DagensDK_930x180_2', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-1').addService(googletag.pubads());
-	googletag.defineSlot('/49662453/DagensDK_930x180_3', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-2').addService(googletag.pubads());
-	googletag.defineSlot('/49662453/DagensDK_930x600_Floating', [930, 600], 'div-DagensDK_930x600_Floating').addService(googletag.pubads());
-	googletag.defineSlot('/49662453/DagensDK_960x600_Floating', [960, 600], 'div-DagensDK_960x600_Floating').addService(googletag.pubads());
-	// googletag.defineSlot('/49662453/DagensDK_Mobile_300x50_1', [[300, 50], [300, 250]], 'div-DagensDK_Mobile_300x50_1').addService(googletag.pubads());
-	// googletag.defineSlot('/49662453/DagensDK_Mobile_300x50_2', [[300, 50], [300, 250]], 'div-DagensDK_Mobile_300x50_2').addService(googletag.pubads());
-	// googletag.defineSlot('/49662453/DagensDK_Mobile_300x50_3', [[300, 50], [300, 250]], 'div-DagensDK_Mobile_300x50_3').addService(googletag.pubads());
-	// googletag.defineSlot('/49662453/DagensDK_Mobile_300x50_Article_1', [[300, 50], [300, 250]], 'div-DagensDK_Mobile_300x50_Article_1').addService(googletag.pubads());
-	// googletag.defineSlot('/49662453/DagensDK_Mobile_300x50_Article_2', [[300, 50], [300, 250]], 'div-DagensDK_Mobile_300x50_Article_2').addService(googletag.pubads());
-	googletag.defineSlot('/49662453/DagensDK_Wallpaper', [960, 600], 'div-DagensDK_Wallpaper').addService(googletag.pubads());
-	googletag.pubads().enableSingleRequest();
-	googletag.enableServices();
-});
-</script>
+    <script type='text/javascript'>
+      googletag.cmd.push(function () {
+        googletag.defineSlot('/49662453/DagensDK_160x600_L', [160, 600], 'div-DagensDK_160x600_L').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDK_160x600_R', [160, 600], 'div-DagensDK_160x600_R').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDk_250x250_Article_1', [250, 250], 'div-DagensDk_250x250_Article_1').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDk_250x250_Article_2', [250, 250], 'div-DagensDk_250x250_Article_2').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDk_250x250_Article_3', [250, 250], 'div-DagensDk_250x250_Article_3').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDk_300x250_1', [300, 250], 'div-DagensDk_300x250_1').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDK_300x250_2', [300, 250], 'div-DagensDK_300x250_2').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDK_930x180_1', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-0').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDK_930x180_2', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-1').addService(googletag.pubads());
+        googletag.defineSlot('/49662453/DagensDK_930x180_3', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-2').addService(googletag.pubads());
+        //FMA// googletag.defineSlot('/49662453/DagensDK_930x600_Floating', [930, 600], 'div-DagensDK_930x600_Floating').addService(googletag.pubads());
+        //FMA// googletag.defineSlot('/49662453/DagensDK_960x600_Floating', [960, 600], 'div-DagensDK_960x600_Floating').addService(googletag.pubads());
+        //FMA// googletag.defineSlot('/49662453/DagensDK_Wallpaper', [960, 600], 'div-DagensDK_Wallpaper').addService(googletag.pubads());
+        googletag.pubads().enableSingleRequest();
+        googletag.enableServices();
+      });
+    </script>
+    <!-- END DFP header scripts on desktop article pages -->
+  <?php } ?>
 
 </head>
 <body<?php print $attributes;?>>
