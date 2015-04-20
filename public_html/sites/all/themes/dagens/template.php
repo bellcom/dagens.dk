a8b22661b36ae3bc627036f68a76ed05c8f8b297
Morten Nissen <mn@bellcom.dk>
2015-04-20 14:11:26 +0200

    Now injecting ad tags on 3. paragraph tag on articles

--- public_html/sites/all/themes/dagens/template.php
+++ public_html/sites/all/themes/dagens/template.php
@@ -14,3 +14,29 @@
  /**
 * preprocess hook to add variables to the node.tpl.php
 */
+
+
+/**
+ * Function used in node template, sends body throug preg_replace. Lets us insert html after first
+ * paragraph in the article.
+ */
+function dagens_insert_ad_on_article($matches)
+{
+    static $count = 0;
+    $ret = $matches[1];
+    if (++$count == 3) {
+        $ret .= <<<EOL
+<!-- adServing 7.1.1c www.dagens.dk + Adform OS 1x1 -->
+<script type="text/javascript">
+var adspaceid = '713';
+var keywords = '';
+if(adServingLoad == undefined) var adServingLoad = '';
+document.write('<scr'+'ipt type="text/javascript" src="http://dinsalgsvagt.adservinginternational.com/js.php?adspaceid='+adspaceid+'&adServingLoad='+adServingLoad+'&keywords='+keywords+'&click="></scr'+'ipt>');
+</script>
+<noscript>
+<iframe src="http://dinsalgsvagt.adservinginternational.com/iframe.php?adspaceid=713&keywords=&click=" frameborder="0" marginheight="0" marginwidth="0" width="1" height="1" scrolling="no" style="width:1px; height:1px; margin:0px;"></iframe>
+</noscript>
+EOL;
+    }
+    return $ret;
+}
