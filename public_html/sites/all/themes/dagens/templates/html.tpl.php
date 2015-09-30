90fcf2f82ec42e9152aa85118500cc6378284286
Morten Nissen <mn@bellcom.dk>
2015-09-30 11:00:10 +0200

    Remove tags from test to

--- public_html/sites/all/themes/dagens/templates/html.tpl.php
+++ public_html/sites/all/themes/dagens/templates/html.tpl.php
@@ -8,40 +8,6 @@
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 
   <?php if(drupal_is_front_page()) { ?>
-      <!-- BEGIN - RTA Script -->
-      <script type='text/javascript'>
-          var crtg_nid = '4205';
-          var crtg_cookiename = 'crtg_rta';
-          var crtg_varname = 'crtg_content';
-
-          function crtg_getCookie(c_name) {
-              var i, x, y, ARRCookies = document.cookie.split(";");
-              for (i = 0; i < ARRCookies.length; i++) {
-                  x = ARRCookies[i].substr(0, ARRCookies[i].indexOf("="));
-                  y = ARRCookies[i].substr(ARRCookies[i].indexOf("=") + 1);
-                  x = x.replace(/^\s+|\s+$/g, "");
-                  if (x == c_name) {
-                      return unescape(y);
-                  }
-              }
-              return '';
-          }
-          var crtg_content = crtg_getCookie(crtg_cookiename);
-          var crtg_rnd = Math.floor(Math.random() * 99999999999);
-          (function () {
-              var crtg_url = location.protocol + '//rtax.criteo.com/delivery/rta/rta.js?netId=' + escape(crtg_nid);
-              crtg_url += '&cookieName=' + escape(crtg_cookiename);
-              crtg_url += '&rnd=' + crtg_rnd;
-              crtg_url += '&varName=' + escape(crtg_varname);
-              var crtg_script = document.createElement('script');
-              crtg_script.type = 'text/javascript';
-              crtg_script.src = crtg_url;
-              crtg_script.async = true;
-              if (document.getElementsByTagName("head").length > 0) document.getElementsByTagName("head")[0].appendChild(crtg_script);
-              else if (document.getElementsByTagName("body").length > 0) document.getElementsByTagName("body")[0].appendChild(crtg_script);
-          })();
-      </script>
-      <!-- END - RTA Script -->
       <!-- BEGIN DFP header scripts on desktop frontpage -->
       <script type='text/javascript'>
           var googletag = googletag || {};
@@ -69,13 +35,7 @@
               googletag.defineSlot('/49662453/TestDagensDK_930x180_1', [[728, 90], [930, 180]], 'div-DagensDK_930x180_1').addService(googletag.pubads());
               googletag.defineSlot('/49662453/TestDagensDK_930x180_2', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-1').addService(googletag.pubads());
               googletag.defineSlot('/49662453/TestDagensDK_930x180_3', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-2').addService(googletag.pubads());
-              googletag.defineOutOfPageSlot('/49662453/DagensDK_InterstitialsOnFrontPage', 'div-DagensDK_InterstitialsOnFrontPage').addService(googletag.pubads());
-              // BEGIN DFP RTA label splitter
-              var crtg_split = crtg_content.split(';');
-              for (var i=1;i<crtg_split.length;i++) {
-                  googletag.pubads().setTargeting("" + (crtg_split[i-1].split('='))[0] + "", "" + (crtg_split[i-1].split('='))[1] + "");
-              }
-              // END DFP RTA label splitter
+              googletag.defineOutOfPageSlot('/49662453/TestDagensDK_InterstitialsOnFrontPage', 'div-DagensDK_InterstitialsOnFrontPage').addService(googletag.pubads());
               googletag.pubads().enableSingleRequest();
               googletag.pubads().collapseEmptyDivs();
               googletag.enableServices();
@@ -84,40 +44,6 @@
       <!-- END DFP header scripts on desktop frontpage -->
   <?php } else { ?>
     <!-- BEGIN DFP header scripts on desktop article pages -->
-      <!-- BEGIN - RTA Script -->
-      <script type='text/javascript'>
-          var crtg_nid = '4205';
-          var crtg_cookiename = 'crtg_rta';
-          var crtg_varname = 'crtg_content';
-
-          function crtg_getCookie(c_name) {
-              var i, x, y, ARRCookies = document.cookie.split(";");
-              for (i = 0; i < ARRCookies.length; i++) {
-                  x = ARRCookies[i].substr(0, ARRCookies[i].indexOf("="));
-                  y = ARRCookies[i].substr(ARRCookies[i].indexOf("=") + 1);
-                  x = x.replace(/^\s+|\s+$/g, "");
-                  if (x == c_name) {
-                      return unescape(y);
-                  }
-              }
-              return '';
-          }
-          var crtg_content = crtg_getCookie(crtg_cookiename);
-          var crtg_rnd = Math.floor(Math.random() * 99999999999);
-          (function () {
-              var crtg_url = location.protocol + '//rtax.criteo.com/delivery/rta/rta.js?netId=' + escape(crtg_nid);
-              crtg_url += '&cookieName=' + escape(crtg_cookiename);
-              crtg_url += '&rnd=' + crtg_rnd;
-              crtg_url += '&varName=' + escape(crtg_varname);
-              var crtg_script = document.createElement('script');
-              crtg_script.type = 'text/javascript';
-              crtg_script.src = crtg_url;
-              crtg_script.async = true;
-              if (document.getElementsByTagName("head").length > 0) document.getElementsByTagName("head")[0].appendChild(crtg_script);
-              else if (document.getElementsByTagName("body").length > 0) document.getElementsByTagName("body")[0].appendChild(crtg_script);
-          })();
-      </script>
-      <!-- END - RTA Script -->
       <!-- BEGIN DFP header scripts on desktop article pages -->
       <script type='text/javascript'>
           var googletag = googletag || {};
@@ -149,12 +75,6 @@
               googletag.defineSlot('/49662453/TestDagensDK_930x180_2', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-1').addService(googletag.pubads());
               googletag.defineSlot('/49662453/TestDagensDK_930x180_3', [[728, 90], [930, 180]], 'div-gpt-ad-1418125366865-2').addService(googletag.pubads());
               googletag.defineOutOfPageSlot('/49662453/DagensDK_InterstitialsOnArticlePages', 'div-DagensDK_InterstitialsOnArticlePages').addService(googletag.pubads());
-              // BEGIN DFP RTA label splitter
-              var crtg_split = crtg_content.split(';');
-              for (var i=1;i<crtg_split.length;i++) {
-                  googletag.pubads().setTargeting("" + (crtg_split[i-1].split('='))[0] + "", "" + (crtg_split[i-1].split('='))[1] + "");
-              }
-              // END DFP RTA label splitter
               googletag.defineSlot('/49662453/TestDagensDK_Thumbnails_Article_1', [[600, 200], [600, 450], [600, 338]], 'div-DagensDK_Thumbnails_Article_1').addService(googletag.pubads());
               googletag.pubads().enableSingleRequest();
               googletag.pubads().collapseEmptyDivs();
